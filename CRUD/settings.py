92bd05497336382d7699c278deffe94a4245f8dd
zainbt <zainbintanveer@gmail.com>
2020-02-05 21:10:59 +0800

    added : Pipfile & setup.py, updated : django_heroku in settings.py

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,3 +146,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # Configure settings
 # CDNJS_STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'cdn')
 # CDNJS_STATIC_URL = '/static/cdn/' # With "/" at end of string
+
+django_heroku.settings(locals())
