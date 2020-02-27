bb5d838c1635cb9aabdd6ea6039ce845290cf1bc
alpha4alpha <hitman.ali123@gmail.com>
2020-02-27 21:35:52 +0500

    Added: Refrences

--- company/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from accounts.models import User
-
-
-class UsersSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['fullname','email','website', 'phone']
